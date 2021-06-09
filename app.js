9ef8af0c224706b44a7fd072f7d74f08b6f3abae
Godwin Ogbomon <aimsnetinc@gmail.com>
2021-06-09 13:53:32 -0400

    test -node

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const path = require('path')
-const http = require('http');
-const adminRoute = require('./routes/admin')
-const shopRoute = require('./routes/shop')
-const app= express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(adminRoute);
-app.use(shopRoute);
+const express = require('express');
+const path = require('path');
+const store = require('./store/datastore');
+const initialStoreData = require('./store/data');
+const Musician = require('./models/musician');
+const musicianRoutes = require('./routes/musician');
 
-app.use((req, resp, next) =>{
-  resp.status(404).send('<h1>Page not found </h2>')
+const app = express();
+const port = process.env.PORT || 3001;
 
+// include routes
+app.use('/musician', musicianRoutes);
+
+app.use(express.static('public'));
+
+// Index route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
-app.listen(3000)
+
+// initialize store
+const musician = new Musician(store);
+musician.initStore(initialStoreData);
+app.locals.musician = musician;
+
+// start server
+const server = app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
+
+module.exports = server;
